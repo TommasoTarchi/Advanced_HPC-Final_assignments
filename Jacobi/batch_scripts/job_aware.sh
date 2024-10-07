307fbf1d20305f12900799f706b6e83d67016799
Tommaso Tarchi <tommaso.tarchi@gmail.com>
2024-10-07 08:51:03 +0200

    Jacobi data on larger grid

--- Jacobi/batch_scripts/job_aware.sh
+++ Jacobi/batch_scripts/job_aware.sh
@@ -4,14 +4,14 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --gpus-per-node=4
 #SBATCH --cpus-per-task=7
-#SBATCH --time=2:00:00
+#SBATCH --time=4:00:00
 #SBATCH --partition=boost_usr_prod
 #SBATCH -A ict24_dssc_gpu
 #SBATCH --output=report_aware.out
 
 
 # choose matrix size and number of threads
-mat_size=1200
+mat_size=30000
 num_threads=7
 
 
